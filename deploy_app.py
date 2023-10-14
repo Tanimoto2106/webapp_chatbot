3bbd0e138e6c769ffe8cc8c026bca7f05a701cb4
Tanimoto2106 <146488785+Tanimoto2106@users.noreply.github.com>
2023-10-14 09:39:18 +0900

    Update deploy_app.py

--- deploy_app.py
+++ deploy_app.py
@@ -11,21 +11,9 @@ st.title("Echo Bot - 家庭教師")
 # Initialize chat history and tokens history
 if "display_messages" not in st.session_state:
     st.session_state.display_messages = []
-if "query_history" not in st.session_state:
-    st.session_state.query_history = []
-if "total_tokens" not in st.session_state:
-    st.session_state.total_tokens = 0
-
-# Display chat messages from display history
-for message in st.session_state.display_messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
 
 # React to user input
 if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container immediately
-    with st.chat_message("user"):
-        st.markdown(prompt)
     # Add user message to display history
     st.session_state.display_messages.append({"role": "user", "content": prompt})
 
@@ -38,27 +26,10 @@ if prompt := st.chat_input("What is up?"):
     )
     response_message = response.choices[0].message['content'].strip()
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response_message)
     # Add assistant response to display history
     st.session_state.display_messages.append({"role": "assistant", "content": response_message})
 
-    # Add to query history
-    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
-    tokens = len(encoding.encode(prompt))
-    st.session_state.query_history.append({"role": "user", "content": prompt, "tokens": tokens})
-    st.session_state.total_tokens += tokens
-
-    tokens = len(encoding.encode(response_message))
-    st.session_state.query_history.append({"role": "assistant", "content": response_message, "tokens": tokens})
-    st.session_state.total_tokens += tokens
-
-    # Ensure total tokens in query history is under 1000
-    while st.session_state.total_tokens > 1000:
-        removed_item = st.session_state.query_history.pop(0)  # Remove oldest entry
-        st.session_state.total_tokens -= removed_item["tokens"]
-
-    # Print query history and tokens for debug purposes
-    for item in st.session_state.query_history:
-        print(f"Role: {item['role']}, Content: {item['content']}, Tokens: {item['tokens']}")
+# Display chat messages from display history
+for message in st.session_state.display_messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
